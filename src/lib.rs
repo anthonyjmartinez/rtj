58d0f0fd026fad33bc822e110349b85746a31b48
Anthony J. Martinez <anthony@ajmartinez.com>
2021-03-26 16:29:30 +0100

    Relicense with MIT / Apache 2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,10 @@
-/*   
-    rtj provides a generic job execution framework in Rust
-    Copyright (C) 2021 Anthony Martinez
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+// rtj provides a generic job execution framework in Rust
+// Copyright 2021 Anthony Martinez
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
 
 #[cfg(test)]
 mod tests {
